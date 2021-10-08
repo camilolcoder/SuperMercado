febd4f60e62d9c36d506935bc37f579bb74faeea
Camilol <j.colmeanares@uniandes.edu.co>
2021-10-07 21:22:56 -0500

    actualizaciones

--- src/Modelo/Factura.java
+++ src/Modelo/Factura.java
@@ -8,5 +8,43 @@ public class Factura {
     private Cliente cliente;
     private int id;
 
+    public Factura(List<Producto> productos, Cliente cliente, int id) {
+        this.productos = productos;
+        this.cliente = cliente;
+        this.id = id;
+    }
 
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTotalPagar()
+    {
+        int total = 0;
+        for(Producto producto: productos)
+        {
+            total += producto.getPrecio();
+        }
+        return total;
+    }
 }
