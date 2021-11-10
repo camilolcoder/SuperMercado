f53484e519a35748c7bf259aafeed7c82cef65e1
camilolcoder <j.colmenares@uniandes.edu.co>
2021-11-10 17:36:56 -0500

    Interfaz del sistema POS creada, falta implementacion

--- src/Interfaz/InterfazPos.java
+++ src/Interfaz/InterfazPos.java
@@ -5,13 +5,19 @@ import java.awt.*;
 
 public class InterfazPos extends JFrame {
 
+    private panelOpcionesPos panelPrincipalPos;
+
     public InterfazPos()
     {
         setTitle("LightsOut");
         setSize(600, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        getContentPane().setBackground(new Color(0, 177, 185));
+        getContentPane().setBackground(new Color(178, 177, 185));
         //setLayout( new BorderLayout() );
+
+        panelPrincipalPos = new panelOpcionesPos(this);
+        add(panelPrincipalPos, BorderLayout.CENTER);
+
         setLocationRelativeTo(null);
         setVisible(true);
     }
---
+++ src/Interfaz/panelOpcionesPos.java
@@ -0,0 +1,59 @@
+package Interfaz;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class panelOpcionesPos extends JPanel implements ActionListener {
+
+    private InterfazPos principalPos;
+
+    public final static String CREATECLIENT = "CREATECLIENT";
+    public final static String CANJEO = "CANJEO";
+    public final static String SALIR = "SALIR";
+
+    private JButton createClient;
+    private JButton canjearCompras;
+    private JButton salir;
+
+    public panelOpcionesPos(InterfazPos PprincipalPos)
+    {
+        principalPos = PprincipalPos;
+        setLayout(new GridLayout(3, 1));
+
+        createClient = new JButton("Crear cliente");
+        createClient.setActionCommand("CREATECLIENT");
+        createClient.addActionListener(this);
+        add(createClient);
+
+        canjearCompras = new JButton("Canjear compras");
+        canjearCompras.setActionCommand("CANJEO");
+        canjearCompras.addActionListener(this);
+        add(canjearCompras);
+
+        salir = new JButton("Salir");
+        salir.setActionCommand("SALIR");
+        salir.addActionListener(this);
+        add(salir);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        String comando = e.getActionCommand();
+        if (comando.equals("CREATECLIENT"))
+        {
+            System.out.println("WORKING BRO");
+        }
+        else if (comando.equals("CANJEO"))
+        {
+            System.out.println("WORKING 2!");
+        }
+        else if (comando.equals("SALIR"))
+        {
+            System.out.println("WORKING 3!");
+        }
+    }
+}
