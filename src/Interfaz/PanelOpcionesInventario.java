3f8d018fb447949e2921f033000de683fa895b15
camilolcoder <j.colmenares@uniandes.edu.co>
2021-11-14 07:47:08 -0500

    Salto entre paginas funcionando - pero error para devolverse a la principal

--- src/Interfaz/PanelOpcionesInventario.java
+++ src/Interfaz/PanelOpcionesInventario.java
@@ -9,17 +9,20 @@ public class PanelOpcionesInventario extends JPanel implements ActionListener {
 
     private InterfazInventario principalInventario;
 
+    public final static String MENUPRIN = "MENUPRIN";
+
     private JButton crearCategoria;
     private JButton crearProducto;
     private JButton crearLote;
     private JButton elimLoteEsp;
     private JButton mostrarLotesVencidos;
+    private JButton menuPrincipal;
 
     public PanelOpcionesInventario(InterfazInventario PprincipalInventario)
     {
         principalInventario = PprincipalInventario;
 
-        setLayout(new GridLayout(5, 1));
+        setLayout(new GridLayout(4, 2));
         crearCategoria = new JButton("Crear categoria");
         add(crearCategoria);
 
@@ -35,10 +38,22 @@ public class PanelOpcionesInventario extends JPanel implements ActionListener {
         mostrarLotesVencidos = new JButton("Mostrar lotes vencidos");
         add(mostrarLotesVencidos);
 
+        menuPrincipal = new JButton("Volver al menu principal");
+        menuPrincipal.setActionCommand("MENUPRIN");
+        menuPrincipal.addActionListener(this);
+        add(menuPrincipal);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        String comando = e.getActionCommand();
+        if (comando.equals("MENUPRIN"))
+        {
+            System.out.println("MENU PRINCIPAL");
+            InterfazPos principalPos = new InterfazPos();
+            principalPos.show();
+            principalInventario.dispose();
+        }
     }
 }
