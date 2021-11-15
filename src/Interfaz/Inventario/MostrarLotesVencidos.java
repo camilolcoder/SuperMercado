805cb9d35121058d6c3fdc65add53bc9b17e8540
camilolcoder <j.colmenares@uniandes.edu.co>
2021-11-15 18:11:47 -0500

    metodo para mostrar informacion en el scroll pane funcionando falta implementacion conectando con base de datos

--- src/Interfaz/Inventario/MostrarLotesVencidos.java
+++ src/Interfaz/Inventario/MostrarLotesVencidos.java
@@ -26,7 +26,15 @@ public class MostrarLotesVencidos extends JDialog implements ActionListener {
         lotesVencidosText = new JLabel("Lotes vencidos");
         add(lotesVencidosText);
 
-        lotesVencidos = new JScrollPane();
+        JPanel infoLotesVencidos = new JPanel();
+        GridLayout gl = new GridLayout(120, 1);
+        infoLotesVencidos.setLayout(gl);
+        for (int i = 0; i < 100; i++)
+        {
+            infoLotesVencidos.add(new JLabel("Lote 2222|2344|323|134355|236466|productos pesados|informacion pesada"));
+        }
+
+        lotesVencidos = new JScrollPane(infoLotesVencidos, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         add(lotesVencidos);
 
         cerrarDialog = new JButton("Cerrar");
